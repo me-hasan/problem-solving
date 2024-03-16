2d134aaa35e53f83ee636da893cccd4827be9d15
khayrul.wingsfin <khhasan@wingsfin.com>
2024-03-16 13:17:09 +0600

    Update

--- algorithm/search/binarySearch.js
+++ algorithm/search/binarySearch.js
@@ -4,22 +4,16 @@ function binary(arr, param){
     var mid = Math.floor((start + end) / 2);
 
     while(arr[mid] !== param && start <= end){
-        if(param < arr[mid]){
-            end = mid - 1;
-        }else{
-            start = mid + 1;
-        }
-        var mid = Math.floor((start + end) / 2);
+        if(param < arr[mid]) end = mid - 1;
+        else start = mid + 1;
+        mid = Math.floor((start + end) / 2);
     }
-    if(arr[mid] === param){
-        return mid;
-    }
-    return -1;
+
+    return arr[mid] === param ?  mid :  -1;
 }
 
 
-let result = binary([1,2,3,5,6,7,8,90], 12);
+let result = binary([1,2,3,5,6,7,8,90], 7);
 
-console.log()
 
 console.log(result);
