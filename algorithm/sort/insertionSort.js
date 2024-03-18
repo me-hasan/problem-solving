a80241f54eaa561959812740c15acd40edcfd6a0
k-hasan <khayrul.web@gmail.com>
2024-03-19 03:48:16 +0600

    Update last commit

--- algorithm/sort/insertionSort.js
+++ algorithm/sort/insertionSort.js
@@ -1,6 +1,5 @@
 function insertionSort(arr) {
-    const n = arr.length;
-    for (let i = 1; i < n; i++) {
+    for (let i = 1; i < arr.length; i++) {
         let current = arr[i];
         let j;
         for (j = i - 1; j >= 0 && arr[j] > current; j--) {
