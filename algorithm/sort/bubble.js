131a984a8c80d6549922771cc004621fa12f5fba
k-hasan <khayrul.web@gmail.com>
2024-03-19 01:30:57 +0600

    Update

--- algorithm/sort/bubble.js
+++ algorithm/sort/bubble.js
@@ -2,19 +2,19 @@ function bubbleShort(arr){
     const swap = (a, b)=> 
     ([arr[b], arr[a]] = [arr[a], arr[b]]);
 
-    var noSwap;
+   
     for (let i = 0; i < arr.length; i++){
-        noSwap = true;
+       
         for (let j = 0; j < arr.length - 1 - i; j++){
             if(arr[j] > arr[j+1]){
                
 
                 swap(j, j+1)
-                noSwap = false;
+              
 
             }
         }
-        if(noSwap) break;
+       
     }
     return arr;
 }
