b5efbcb8ddd2a0723b61bc7ae65b598cfe496937
k-hasan <khayrul.web@gmail.com>
2024-03-29 19:33:08 +0600

    add singly linked list

---
+++ dataStructures/SinglyLinkList.js
@@ -0,0 +1,36 @@
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode
+        }
+        this.length ++
+        return this
+    } 
+}
+
+
+var list = new SinglyLinkedList();
+list.push('Hello');
+list.push('Man');
+list.push('Man1');
+list.push('Khayrul');
+
+console.log(list)
