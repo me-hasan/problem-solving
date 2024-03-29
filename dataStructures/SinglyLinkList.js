e8c8b478d19a5c96fe0eb1b229d6d70ea42be1a0
k-hasan <khayrul.web@gmail.com>
2024-03-30 05:24:05 +0600

    add pop function

--- dataStructures/SinglyLinkList.js
+++ dataStructures/SinglyLinkList.js
@@ -21,9 +21,28 @@ class SinglyLinkedList{
             this.tail.next = newNode;
             this.tail = newNode
         }
-        this.length ++
+        this.length++;
         return this
     } 
+
+
+    pop(){
+        if(!this.head) return undefined;
+        var current = this.head;
+        var newTail = current;
+            while(current.next){
+                newTail = current;
+                current = current.next;
+            }
+            this.tail = newTail;
+            this.tail.next = null;
+            this.length--;
+            if(this.length === 0){
+                this.head = null;
+                this.tail = null;
+            }
+            return current;
+    }
 }
 
 
@@ -32,5 +51,9 @@ list.push('Hello');
 list.push('Man');
 list.push('Man1');
 list.push('Khayrul');
+list.pop();
+list.pop();
+list.pop();
+
 
 console.log(list)
